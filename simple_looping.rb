091d71b1e7adce708f065301e590ff950ee029a8
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-19 17:43:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   # phrase = "Welcome to Flatiron School's Web Development Course!"
 
-end
+
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword

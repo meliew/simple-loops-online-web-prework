93002d5e3eee2c515a74584bc956aa0baf35648e
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-19 17:54:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,8 +17,11 @@ end
 
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times
+  puts phrase
+end
+  # code your solution here using the "times" keyword
 
 end
 
